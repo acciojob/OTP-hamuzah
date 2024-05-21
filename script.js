a7073ff6a10698eb77488a54d3c20c3430106720
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-05-21 17:27:58 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function focusNext(currentInput) {
+    if (currentInput.value.length === 1) {
+        const nextInput = currentInput.nextElementSibling;
+        if (nextInput) {
+            nextInput.focus();
+        }
+    }
+}
+
+function handleBackspace(currentInput, event) {
+    if (currentInput.value === '' && event.key === 'Backspace') {
+        const previousInput = currentInput.previousElementSibling;
+        if (previousInput) {
+            previousInput.focus();
+        }
+    }
+}
