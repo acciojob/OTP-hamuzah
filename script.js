43a347738eada1c5d8dfb56aeb7dcfc0add0e7d5
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-06-17 10:50:42 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
- const codeInputs = document.querySelectorAll(".code");
+//your JS code here. If required.
+function focusNext(currentInput) {
+    if (currentInput.value.length === 1) {
+        const nextInput = currentInput.nextElementSibling;
+        if (nextInput) {
+            nextInput.focus();
+        }
+    }
+}
 
-        codeInputs.forEach((input, index) => {
-            input.addEventListener("input", (event) => {
-                const inputValue = event.target.value;
-
-                if (inputValue.length === 1) {
-                    if (index < codeInputs.length - 1) {
-                        codeInputs[index + 1].focus();
-                    }
-                }
-            });
-
-            input.addEventListener("keydown", (event) => {
-                if (event.key === "Backspace" && index > 0) {
-                    codeInputs[index - 1].focus();
-                }
-            });
-        });
+function handleBackspace(currentInput, event) {
+    if (currentInput.value === '' && event.key === 'Backspace') {
+        const previousInput = currentInput.previousElementSibling;
+        if (previousInput) {
+            previousInput.focus();
+        }
+    }
+}
